f475b8c2c5797631c9b3c961aae61c629b94baba
pokidyuk <pokidyuk@MR012>
2023-03-21 16:58:45 +0300

    Loops

--- Loops/main.cpp
+++ Loops/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<conio.h>
 using namespace std;
 
 using std::cin;
@@ -7,6 +8,7 @@ using std::endl;
 
 //#define LOOPS
 //#define Home_W
+//#define WHILE_1
 
 void main()
 {
@@ -49,6 +51,8 @@ void main()
 
 #endif // Home_W
 
+#ifdef WHILE_1
+
 	int number;
 	cout << "Введите число: "; cin >> number;
 	int buffer = number;
@@ -64,4 +68,26 @@ void main()
 	cout << number << endl;
 	cout << buffer << endl;
 	cout << reverse << endl;
+
+#endif // WHILE_1
+
+	char key;		//хранит код нажатой клавиши
+	do
+	{
+		key = _getch();
+		//cout << (int)key << "\t" << key << endl;
+
+		switch (key)
+		{
+		case'w': cout << key << "\t" << "Вперед" << endl; break;
+		case's': cout << key << "\t" << "Назад" << endl; break;
+		case'a': cout << key << "\t" << "Влево" << endl; break;
+		case'd': cout << key << "\t" << "Вправо" << endl; break;
+		case'esc':cout << "Выход" << endl; break;					//не работает!!!
+		default:cout << "Error!";
+		}
+
+	} while (key != 27);
+
+
 }
---
+++ Palindrome/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int namber;
+	cout << "¬ведите номер билета: "; cin >> namber;
+
+	int buffer = namber;
+}
