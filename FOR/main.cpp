0af897141b59a976815183f8636951dd1134beae
pokidyuk <pokidyuk@MR012>
2023-03-28 16:28:26 +0300

    for

---
+++ FOR/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+
+	cout << "Введите количество итераций: "; cin >> n;
+
+	for (int i = 0;	i < n;i++)
+	{
+		cout << n << "\t";
+	}
+	cout << endl;
+}
--- Shooter/main.cpp
+++ Shooter/main.cpp
@@ -38,19 +38,23 @@ void main()
 		{
 		case'w':case'ц': cout << key << "\t" << "Вперед" << endl; break;
 		case KEYS::W:case KEYS::Ц:cout << key << "\t" << "Бег вперед" << endl; break;
+
 		case's':case'ы': cout << key << "\t" << "Назад" << endl; break;
 		case KEYS::S:case KEYS::Ы:cout << key << "\t" << "Разворот на 180 и по тапкам" << endl; break;
+
 		case'a':case'ф': cout << key << "\t" << "Влево" << endl; break;
 		case KEYS::A:case KEYS::Ф: cout << key << "\t" << "Кувырок влево" << endl; break;
+
 		case'd':case'в': cout << key << "\t" << "Вправо" << endl; break;
 		case KEYS::D:case KEYS::В: cout << key << "\t" << "Кувырок вправо" << endl; break;
-		case KEYS::ENTER:case KEYS::SPACE:cout << KEYS::ENTER << "\t" << "Огонь" << endl; break;
+
+		case KEYS::ENTER:cout << KEYS::ENTER <<  "\t" << "Огонь" << endl; break;
+		case KEYS::SPACE:cout << KEYS::SPACE << "\t" << "Прыжок" << endl; break;
 
 		case KEYS::ARROW_FORWARD:cout << KEYS::ARROW_FORWARD << "\t" << "Движение вперед" << endl; break;
 		case KEYS::ARROW_BACK:cout << KEYS::ARROW_BACK << "\t" << "Движение назад" << endl; break;
 		case KEYS::ARROW_LEFT:cout << KEYS::ARROW_LEFT << "\t" << "Движение влево" << endl; break;
 		case KEYS::ARROW_RIGHT:cout << KEYS::ARROW_RIGHT << "\t" << "Движение вправо" << endl; break;
-
 
 		case KEYS::ESC:cout << KEYS::ESC << "\t" << "Выход" << endl; exit(0);
 
